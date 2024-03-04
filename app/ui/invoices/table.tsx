602b697bdbcc814b4b924af197a089d0bd3564fc
常德恩 <changdeen@sailvan.com>
2024-03-04 14:05:08 +0800

    提交

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,7 +11,8 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
-  const invoices = await fetchFilteredInvoices(query, currentPage);
+  // const invoices = await fetchFilteredInvoices(query, currentPage);
+  const invoices:[] = []
 
   return (
     <div className="mt-6 flow-root">
